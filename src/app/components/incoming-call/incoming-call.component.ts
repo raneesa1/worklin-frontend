ef9ad2828e9f04f25f65aca3ebaf5a03e61d6a64
raneesa <raneesak73@gmail.com>
2024-10-11 19:14:42 +0530

    conditional rendering of incoming call

--- src/app/components/incoming-call/incoming-call.component.ts
+++ src/app/components/incoming-call/incoming-call.component.ts
@@ -1,38 +1,22 @@
-import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import {
-  GlobalIncomingCallService,
-  IncomingCallState,
-} from '../../shared/service/GlobalIncomingCall.service';
-
 
 @Component({
   selector: 'app-incoming-call',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './incoming-call.component.html',
-  styles: [
-    `
-      @keyframes slideUp {
-        from {
-          transform: translateY(100%);
-          opacity: 0;
-        }
-        to {
-          transform: translateY(0);
-          opacity: 1;
-        }
-      }
-
-      .animate-slide-up {
-        animation: slideUp 0.3s ease-out forwards;
-      }
-    `,
-  ],
+  styleUrl: './incoming-call.component.scss',
 })
 export class IncomingCallComponent {
   @Input() callerName: string = '';
-  @Output() onAccept = new EventEmitter<void>();
-  @Output() onReject = new EventEmitter<void>();
+  @Output() accept = new EventEmitter<void>();
+  @Output() reject = new EventEmitter<void>();
+
+  acceptCall() {
+    this.accept.emit();
+  }
+
+  rejectCall() {
+    this.reject.emit();
+  }
 }
