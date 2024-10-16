02a27c749582de9811c3d3e6aa44037cbc591de3
raneesa <raneesak73@gmail.com>
2024-10-16 20:29:04 +0530

    changed the google singup button style

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
